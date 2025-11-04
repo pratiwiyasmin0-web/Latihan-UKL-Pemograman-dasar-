04260a8df9f5a529d44c14a926e51ad0977cd4e8
pratiwiyasmin0-web <pratiwiyasmin0@gmail.com>
2025-11-04 09:46:29 +0700

    Remove unused matrix operations from Soal3Sulit.java

--- Soal3Sulit.java
+++ Soal3Sulit.java
@@ -13,46 +13,7 @@ public class Soal3Sulit {
         }
         double rata = total / jumlah;
         System.out.println("Rata-rata nilai = " +rata);
-
-        //jawaban 2
-        System.out.println("\n");
-        System.out.print("Masukkan jumlah baris: ");
-        int baris = masukan.nextInt();
-        System.out.print("Masukkan jumlah kolom: ");
-        int kolom = masukan.nextInt();
-        int[][] A = new int[baris][kolom];
-        int[][] B = new int[baris][kolom];
-        int[][] C = new int[baris][kolom];
-        System.out.println("\n");
-        System.out.println("Masukan Matriks A : ");
-        for (int i = 0; i < baris; i++) {
-            for (int j = 0; j < kolom; j++) {
-                System.out.print("A[" + i + "][" + j + "]: ");
-                A[i][j] = masukan.nextInt();
-            }
-        }
-        System.out.println("\n");
-         System.out.println("Masukan Matriks B :");
-        for (int i = 0; i < baris; i++) {
-            for (int j = 0; j < kolom; j++) {
-                System.out.print("B[" + i + "][" + j + "]: ");
-                B[i][j] = masukan.nextInt();
-            }
-        }
-        // Proses penjumlahan
-        for (int i = 0; i < baris; i++) {
-            for (int j = 0; j < kolom; j++) {
-                C[i][j] = A[i][j] + B[i][j];
-            }
-        }
-        System.out.println("\n");
-         System.out.println("Hasil Penjumlahan Matriks C : ");
-        for (int i = 0; i < baris; i++) {
-            for (int j = 0; j < kolom; j++) {
-                System.out.print(C[i][j] + " ");
-            }
-            System.out.println();
  }
 }
-}
+
 
